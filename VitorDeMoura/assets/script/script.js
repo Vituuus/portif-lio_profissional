299f07f5f223963b259f638255b674cbd0280206
Vituuus <demouravitor2047@gmail.com>
2025-10-12 03:03:36 -0300

    Mobile First Development

--- VitorDeMoura/assets/script/script.js
+++ VitorDeMoura/assets/script/script.js
@@ -1,2 +1,12 @@
 const d = new Date();
 document.getElementById("idadeAtual").innerHTML = d.getFullYear() - 1999;
+
+function menuMobile() {
+     let x = document.getElementById("linkNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+    
+}
